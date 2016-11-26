b12b2cc03eff5076ed1e649e900697c6c24c0c6d
alexzc4 <alexzc@mail.ru>
2016-11-26 08:23:43 +0300

    Поправил сайдбар

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,3 @@
-
 var cronosApp = angular.module('cronosApp', ['ngMaterial','ngRoute','slick','angular-loading-bar','ngAnimate','cfp.loadingBar','ngSanitize'])
 
   cronosApp.controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
@@ -10,7 +9,7 @@ var cronosApp = angular.module('cronosApp', ['ngMaterial','ngRoute','slick','ang
         $mdSidenav(componentId).toggle();
       }
     }
-  })
+  });
 
   cronosApp.config(function($routeProvider) {
     $routeProvider
@@ -37,53 +36,30 @@ var cronosApp = angular.module('cronosApp', ['ngMaterial','ngRoute','slick','ang
       redirectTo: '/contacts'
     });
 
-  })
+  });
 
-  cronosApp.run(function($rootScope, $templateCache) {
-   $rootScope.$on('$viewContentLoaded', function() {
-      $templateCache.removeAll();
-   });
-});
+  cronosApp.controller('contactsCtrl', function ($scope, $templateCache) {
+    $templateCache.removeAll();
+  });
 
-
-    cronosApp.controller('contactsCtrl', function ($scope, $templateCache) {
-      $templateCache.removeAll();
-    })
-
-    cronosApp.controller('projectsCtrl',['$scope','$http', '$location','$sce', function($scope, $http, $location,$sce) {
-      $http.get('pages/projects/projects.json?ver=1').success(function(data, status, headers, config) {
-        $scope.projects = data;
-      });
-
-    }]);
-
-
-    cronosApp.controller('workCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
-      $http.get('pages/how-we-work/how-we-work.json').success(function(data, status, headers, config) {
-        $scope.work = data;
-      })
-    }]);
-
-    cronosApp.controller('projectDetailCtrl',['$scope','$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
-        $scope.projectId = $routeParams.projectId;
-        var url = 'pages/projects/'+$routeParams.projectId+'.json';
-
-        $http.get(url).success(function(data) {
-          $scope.project = data;
-        });
-
-
-        
-
-    }]);
-
-    cronosApp.directive('includeReplace', function () {
-        return {
-            require: 'ngInclude',
-            restrict: 'A', /* optional */
-            link: function (scope, el, attrs) {
-                el.replaceWith(el.children());
-            }
-        };
+  cronosApp.controller('projectsCtrl',['$scope','$http', '$location','$sce', function($scope, $http, $location,$sce) {
+    $http.get('pages/projects/projects.json').success(function(data, status, headers, config) {
+      $scope.projects = data;
     });
 
+  }]);
+
+  cronosApp.controller('workCtrl',['$scope','$http', '$location', function($scope, $http, $location) {
+    $http.get('pages/how-we-work/how-we-work.json').success(function(data, status, headers, config) {
+      $scope.work = data;
+    })
+  }]);
+
+  cronosApp.controller('projectDetailCtrl',['$scope','$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+      $scope.projectId = $routeParams.projectId;
+      var url = 'pages/projects/'+$routeParams.projectId+'.json';
+
+      $http.get(url).success(function(data) {
+        $scope.project = data;
+      });
+  }]);

76da2b0f0417b00fb48bc67776a3c410c27ba394
alex <alexzc@mail.ru>
2016-12-08 14:16:54 +0100

    Добавил переключатель языков в desktop версии

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,6 +20,7 @@ var cronosApp = angular.module('cronosApp', ['ngMaterial','ngAnimate','ngRoute',
     $scope.getClass = function (path) {
       return ($location.path().substr(0, path.length) === path) ? 'active' : '';
     }
+
   })
 
   cronosApp.controller('FooterController', function ($scope, $location) 
